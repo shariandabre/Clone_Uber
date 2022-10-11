8909d8809a94f9b5b00a0e3f2cb5c14c556763dd
shariandabre <94830605+shariandabre@users.noreply.github.com>
2022-10-11 20:21:54 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,7 +8,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDUzekU-QgjiG72nyWTLg2EE5kAyYYcoHs",
+  apiKey: "",
   authDomain: "uberclone-4c95d.firebaseapp.com",
   projectId: "uberclone-4c95d",
   storageBucket: "uberclone-4c95d.appspot.com",
